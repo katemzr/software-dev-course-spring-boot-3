0ef8d952eaa863c4faaa320e83b2039971047f41
katemzr <katemzr@gmail.com>
2026-08-19 17:12:30 -0500

    completed group practice. managed to get program to run

--- src/main/java/com/example/springBoot2/models/Movie.java
+++ src/main/java/com/example/springBoot2/models/Movie.java
@@ -1,6 +1,15 @@
 package com.example.springBoot2.models;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
 public class Movie  {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
     private String name;
     private int year;
     private String director;
@@ -46,5 +55,13 @@ public class Movie  {
 
     public void setRuntime(int runtime) {
         this.runtime = runtime;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
     }
 }
